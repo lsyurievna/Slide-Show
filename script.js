4c1aa6b0496d6c7b01fb0d8fc02c50adbaa103b9
lsyurievna <lsyurievna@mun.ca>
2024-12-19 13:41:33 +0300

    The program is ready

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 $(document).ready(function(){ //document has to be ready firs? why...
     $('.next').on('click', function(){
-        console.log("hi");
+        var currentImg = $('.active');
+        var nextImg = currentImg.next();
+
+        if(nextImg.length){
+            currentImg.removeClass('active').css('z-index', -10);
+            nextImg.addClass('active').css('z-index', 10);
+        }
+    });
+});
+
+$(document).ready(function(){
+    $('.prev').on('click', function(){
+        var currentImg = $('.active');
+        var prevImg = currentImg.prev();
+
+        if(prevImg.length){
+            currentImg.removeClass('active').css('z-index', -10);
+            prevImg.addClass('active').css('z-index', 10);
+        }
     });
 });
